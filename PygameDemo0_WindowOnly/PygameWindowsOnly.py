a77ff1fa6d5541baf7b9124f193f2fd3b5bda1e7
Sertas79 <unitflash79@yandex.ru>
2025-05-12 20:25:59 +0300

    PygameMoveByKeyboardOncePerKey

--- PygameDemo0_WindowOnly/PygameWindowsOnly.py
+++ PygameDemo0_WindowOnly/PygameWindowsOnly.py
@@ -14,6 +14,10 @@ FRAMES_PER_SECOND = 30
 BALL_WIDTH_HEIGHT = 100
 MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
 MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
+TARGET_X = 400
+TARGET_Y = 320
+TARGET_WIDTH_HEIGHT= 120
+N_PIXELS_TO_MOVE = 3
 
 #3 - Инициализируем окружение pygame
 pygame.init()
@@ -22,11 +26,12 @@ clock = pygame.time.Clock()
 
 #4 - Загружаем элементы: изображения, звуки и т.д.
 ballimage = pygame.image.load('images/ball.png')
+targetImage = pygame.image.load('images/target.jpg')
 
 #5 - Инициализируем переменные
 ballX = random.randrange(MAX_WIDTH)
 ballY = random.randrange(MAX_HEIGHT)
-ballRect = pygame.Rect(ballX, ballY, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+targetRect = pygame.Rect(TARGET_X, TARGET_Y, TARGET_WIDTH_HEIGHT, TARGET_WIDTH_HEIGHT)
 
 #6 - Бесконечный цикл
 while True:
@@ -37,28 +42,28 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-        # определяем, щелкну ли пользователь
-        if event.type == pygame.MOUSEBUTTONUP:
-            # mouseX, mouseY = event.pos
-            # Могли бы это сделать при необходимости
-
-            # проверяем, был ли щелчок в пределах прямоугольника мяча
-            # Если это так, выбираем случайны образом новое
-            # местоположение
-            if ballRect.collidepoint(event.pos):
-                ballX = random.randrange(MAX_WIDTH)
-                ballY = random.randrange(MAX_HEIGHT)
-                ballRect = pygame.Rect(ballX, ballY,
-                                       BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
-
+        # определяем, нажал ли пользователь клавишу
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ballX -= N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_RIGHT:
+                ballX += N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_UP:
+                ballY -= N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_DOWN:
+                ballY += N_PIXELS_TO_MOVE
 
     #8 - Выполняем действия "в рамках фрейма"
+    # определяем, перекрывает ли мяч целевое изображение
+    ballRect = pygame.Rect(ballX, ballY, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+    if ballRect.colliderect(targetRect):
+        print('Ball is touching the target')
 
     #9 - Рисуем все элементы окна
     window.fill(BLACK)
 
     #10 - Рисуем все элементы окна
+    window.blit(targetImage, (TARGET_X, TARGET_Y))
     # рисуем мяч на позиции 100 вдоль (x) и 200 вниз по (y)
     window.blit(ballimage, (ballX, ballY))
 
